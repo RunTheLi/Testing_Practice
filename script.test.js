3c795d6af3c669524300e5a7335f5c33e9114927
RunTheLi <lanmarock@gmail.com>
2024-12-24 11:55:52 +0700

    add code

--- script.test.js
+++ script.test.js
@@ -1,5 +1,5 @@
-const sum = require('./sum');
+import { capitalize } from "./script.js"
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('returns it with the first character capitalized', () => {
+  expect(capitalize('hello')).toBe('Hello'); // Fix the expected value
 });
